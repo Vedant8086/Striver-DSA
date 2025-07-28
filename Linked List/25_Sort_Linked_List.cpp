bfb8c5555aa7a5bd1fe25bf2b110a61e9ed50a25
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2025-07-28 23:57:33 +0530

    Commit

--- Linked List/25_Sort_Linked_List.cpp
+++ Linked List/25_Sort_Linked_List.cpp
@@ -24,7 +24,28 @@ class Node {
             this->next = nullptr;
         }
 };
+
+void TraversingLL(Node* head) {
+    while(head != nullptr) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "nullptr" << endl;
+}
+void ArrayToLL(vector<int>& arr,Node*& head) {
+    Node* temp = head;
+    int n = arr.size();
+    for(int i = 1;i < n;i++) {
+        Node* node = new Node(arr[i]);
+        temp->next = node;
+        temp = node;
+    }
+}
 int main() {
+    vector<int> v = {1,2,3,4,5,6,7,8,9};
+    Node* head = new Node(v[0]);
+    Node* temp = head;
+    int n = v.size();
 
     return 0;
 }
