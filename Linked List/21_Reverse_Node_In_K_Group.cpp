ac1579f5ec7344311f2f4e00dd2097d94f6ab1a6
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2025-07-30 00:12:29 +0530

    Commit

--- Linked List/21_Reverse_Node_In_K_Group.cpp
+++ Linked List/21_Reverse_Node_In_K_Group.cpp
@@ -14,9 +14,82 @@
 #include<stdexcept>
 #include<fstream>
 using namespace std;
+class Node {
+    public : 
+        int data;
+        Node* next;
 
+        Node(int data) {
+            this->data = data;
+            this->next = nullptr;
+        }
+};
 
+Node* ReverseLinkedList(Node* head) {
+   Node* temp = head;  
+   
+   Node* prev = NULL;  
+   
+   while(temp != NULL){  
+       Node* front = temp->next;  
+       
+       temp->next = prev;  
+       prev = temp;  
+       
+       temp = front; 
+   }
+   
+   return prev;  
+}
+
+Node* getKthNode(Node* temp, int k){
+    k -= 1; 
+
+    while(temp != NULL && k > 0){ 
+        k--; 
+        temp = temp -> next; 
+    }
+    
+    return temp; 
+}
+
+Node* kReverse(Node* head, int k){
+    Node* temp = head; 
+    Node* prevLast = NULL; 
+    while(temp != NULL){
+        Node* kThNode = getKthNode(temp, k); 
+        if(kThNode == NULL){ 
+            if(prevLast){
+                prevLast -> next = temp; 
+            }
+            break; 
+        }
+        Node* nextNode = kThNode -> next; 
+
+        kThNode -> next = NULL; 
+
+        ReverseLinkedList(temp); 
+       
+        if(temp == head){
+            head = kThNode;
+        }else{
+          
+            prevLast -> next = kThNode; 
+        }
+
+        prevLast = temp; 
+
+        temp = nextNode; 
+    }
+    
+  
+    return head; 
+}
 int main() {
+    vector<int> arr = {1,2,3,4,5,6,7,8,9};
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+    int n = arr.size();
 
     return 0;
 }
--- Linked List/22_Rotate_Linked_List.cpp
+++ Linked List/22_Rotate_Linked_List.cpp
@@ -14,8 +14,46 @@
 #include<stdexcept>
 #include<fstream>
 using namespace std;
+class Node {
+    public : 
+        int data;
+        Node* next;
 
+        Node(int data) {
+            this->data = data;
+            this->next = nullptr;
+        }
+};
+Node* findKNode(Node* head,int k) {
+    Node* temp = head;
+    while(temp != nullptr && k > 0) {
+        temp = temp->next;
+        k--;
+    }
+    return temp;
+
+}
+
+Node* rotate(Node*& head,int k) {
+    if(head == nullptr || head->next == nullptr) {
+        return head;
+
+    }
+    Node* temp = head;
+    int len = 1;
+    while(temp->next != nullptr) {
+        temp = temp->next;
+        len++;
+    }
+    temp->next = head;
+    k = k % len;
+    if(k % len == 0) return head;
+    Node* new_node = findKNode(head,len-k);
+    head = new_node->next;
+    new_node->next = nullptr;
+    return head;
+}
 int main() {
-    
+
     return 0;
 }
