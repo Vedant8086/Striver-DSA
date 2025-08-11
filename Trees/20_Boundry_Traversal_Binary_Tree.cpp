bd219b19dc52a7883b7cb6d8895cca0c08b2ed6f
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2025-08-11 19:01:04 +0530

    Commit Via VSCode

--- Trees/20_Boundry_Traversal_Binary_Tree.cpp
+++ Trees/20_Boundry_Traversal_Binary_Tree.cpp
@@ -23,6 +23,12 @@ class TreeNode {
 };
 
 int main() {
-    
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->left->right->left = new TreeNode(5);
+    root->left->right->right = new TreeNode(6);
     return 0;
 }
