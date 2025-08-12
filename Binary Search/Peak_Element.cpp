68b341bf364e76be8445be33bb7f5193e1c4d9c6
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2025-08-12 23:40:52 +0530

    Commit Via VSCode

--- Binary Search/Peak_Element.cpp
+++ Binary Search/Peak_Element.cpp
@@ -15,10 +15,26 @@
 #include<fstream>
 using namespace std;
 class Solution {
-    public :    
-        int Peak_Element(vector<int> arr) {
-            
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
+        return -1; 
+    }
 };
 int main() {
     int n;
@@ -31,6 +47,8 @@ int main() {
         arr.push_back(temp);
     }
     Solution s1;
-    s1.Peak_Element(arr);
+    int peakIndex = s1.findPeakElement(arr);
+    cout << "Peak Element Index: " << peakIndex << endl;
+    
     return 0;
 }
